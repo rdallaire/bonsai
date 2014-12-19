c92e0ae8d604a166b9707eba03ce2863d2549d33
Ross Dallaire <rossdallaire@gmail.com>
2014-12-19 00:34:16 -0800

    notes

--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 <?php
 
+	// files to load into functions.php
 	$bonsai_includes = array(
 
-		'lib/admin.php'			// admin functions
+		'lib/init.php',				// initial theme setup
+		'lib/assets.php',			// scripts and styles
+		'lib/admin.php' 			// admin functions
 
 	);
 
+	// go through each file and require it
 	foreach ( $bonsai_includes as $file ) {
 
 		if (!$filepath = locate_template($file)) {
