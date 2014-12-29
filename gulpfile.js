6e536e8f4bd8ff784435c28edc288543ca4262a6
Ross Dallaire <rossdallaire@gmail.com>
2014-12-28 22:10:30 -0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,92 @@
-var sass            = require('gulp-sass');
-var sourcemaps      = require('gulp-sourcemaps');
-var minifyCSS       = require('gulp-minify-css');
-// var plugin 		= require('gulp-load-plugins'); 	// loads gulp- plugins from package.json
+var gulp 			= require('gulp');
 
-// task for all css
+// loads gulp- plugins from package.json
+var plugins 		= require('gulp-load-plugins')({ camelize: true });
+
+var theme: {
+
+	name: 'theme'
+
+},
+
+styles: {
+
+	src: 	'./src/assets/styles/main.scss',
+	dist: 	'./dist/assets/styles/',
+	build: 	'./build/' + theme.name + '/assets/styles/'
+
+},
+
+scripts: {
+
+	src: 	'coming soon',
+	dist: 	'coming soon',
+	build: 	'coming soon'
+
+}
+
+images: {
+
+	src: 	'./src/assets/images/',
+	dist: 	'./dist/assets/images/',
+	build: 	'./build/assets/images/'
+
+};
+
+// build styles
 gulp.task('styles', function() {
 
-	gulp.src('./assets-dev/styles/main.scss')   // grab main sass file
-		.pipe(sourcemaps.init())                // start sourcemaps
-		.pipe(sass())                           // run sass compile
-		.pipe(minifyCSS())                      // minify css
-		.pipe(sourcemaps.write('.'))            // write sourcemaps to new file
-		.pipe(gulp.dest('./assets/styles/'));   // output css and sourcemaps
+	gulp.src(styles.src)			// grab main sass file
+		.pipe(plugins.sourcemaps.init())                // start sourcemaps
+		.pipe(plugins.sass())                           // run sass compile
+		.pipe(plugins.minifyCss())                      // minify css
+		.pipe(plugins.sourcemaps.write('.'))            // write sourcemaps to new file
+		.pipe(gulp.dest(styles.dist));		// output css and sourcemaps
 
 });
+
+
+// dist styles
+gulp.task('styles-dist', function() {
+
+});
+
+
+// build scripts
+gulp.task('scripts', function() {
+
+});
+
+// dist scripts
+gulp.task('scripts-dist', function() {
+
+});
+
+// build images
+gulp.task('images', function() {
+
+});
+
+// dist images
+gulp.task('images-dist', function() {
+
+});
+
+// clean the dist and build directories
+gulp.task('clean', function() {
+
+});
+
+// watch for file changes
+gulp.task('watch', function() {
+
+});
+
+// setup the build version of the theme
+gulp.task('build', function() {
+
+});
+
+// set the default task to build
+gulp.task('default', ['build']);
+gulp.task('dist', ['clean', 'styles-dist'])
