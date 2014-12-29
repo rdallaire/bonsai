f9e6b443f35893f692b73ae3898510c5de064df8
Ross Dallaire <rossdallaire@gmail.com>
2014-12-28 19:03:44 -0800

    update file structure

--- 404.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<main role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-	<h2>404 Page Not Found</h2>
-
-</main>
-
-<?php get_footer(); ?>
--- CHANGELOG.md
+++
@@ -1,2 +0,0 @@
-**0.0.1** — 12/8/2014
-* Initial setup/ fork from bones
