9789cf6d8c8aac117c1d05cecc5d3ff3ce492676
Ross Dallaire <rossdallaire@gmail.com>
2015-01-01 09:26:09 -0800

    updat todo and remove comments

--- TODO.md
+++ TODO.md
@@ -1,15 +1,11 @@
 # Todo
 
-* [ ] Configure gulpfile
+* [ ] finish gulpfile
 * [ ] add favicons codes
 * [ ] add woocommerce templates
-* [ ] setup SASS
-* [ ] setup assets file structure
 * [ ] add screenshot.png
 * [ ] pass wordpress theme test
 * [ ] make sure themeforest rules are accounted for [themeforest](https://help.market.envato.com/hc/en-us/articles/202822450-WordPress-Theme-Submission-Requirements)
-* [ ] remove all class names/ IDs
-* [ ] cleanup post types
 * [ ] add comments.php
-* [ ] setup wp_enque and cleanup scripts
 * [ ] should an unminified version of the styles/ js be provided?
+* [ ] setup js and wp_enque for js. Also setup way to grab vendor js and minify
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
-var gulp 			= require('gulp');
-var del 			= require('del');
-
-// loads gulp- plugins from package.json
-var plugins 		= require('gulp-load-plugins')({ camelize: true });
+var gulp            = require('gulp');
+var del             = require('del');
+var plugins         = require('gulp-load-plugins')({ camelize: true });
 
 theme = {
 
@@ -12,41 +10,41 @@ theme = {
 
 styles = {
 
-	src: 	'./src/assets/styles/main.scss',
-	build: 	'./build/assets/styles/',
-	dist: 	'./dist/' + theme.name + '/assets/styles/'
+	src:    './src/assets/styles/main.scss',
+	build:  './build/assets/styles/',
+	dist:   './dist/' + theme.name + '/assets/styles/'
 
 },
 
 scripts = {
 
-	src: 	'./src/assets/scripts/**/*',
-	build: 	'./build/assets/scripts/',
-	dist: 	'./dist/' + theme.name + '/assets/scripts/'
+	src:    './src/assets/scripts/**/*',
+	build:  './build/assets/scripts/',
+	dist:   './dist/' + theme.name + '/assets/scripts/'
 
 },
 
 images = {
 
-	src: 	'./src/assets/images/',
-	build: 	'./build/assets/images/',
-	dist: 	'./dist/' + theme.name + 'assets/images/'
+	src:    './src/assets/images/',
+	build:  './build/assets/images/',
+	dist:   './dist/' + theme.name + 'assets/images/'
 
 };
 
 // build styles
 gulp.task('styles', function() {
 
-	gulp.src(styles.src)						// grab main sass file
-		.pipe(plugins.sourcemaps.init())		// start sourcemaps
-		.pipe(plugins.sass())					// run sass compile
-		.pipe(plugins.autoprefixer({			// auto-prefix css
+	gulp.src(styles.src)
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass())
+		.pipe(plugins.autoprefixer({
 			browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
 		}))
-		.pipe(plugins.minifyCss())				// minify css
-		.pipe(plugins.sourcemaps.write('.'))	// write sourcemaps to new file
-		.pipe(gulp.dest(styles.build))			// output css and sourcemaps
-		.pipe(plugins.rename({suffix: '.min'}));// rename file to main.min.css
+		.pipe(plugins.minifyCss())
+		.pipe(plugins.sourcemaps.write('.'))
+		.pipe(gulp.dest(styles.build))
+		.pipe(plugins.rename({suffix: '.min'}));
 
 });
 
@@ -54,11 +52,11 @@ gulp.task('styles', function() {
 // dist styles
 gulp.task('styles-dist', function() {
 
-	gulp.src(styles.src)						// grab main sass file
-		.pipe(plugins.sass())					// run sass compile
-		.pipe(plugins.minifyCss())				// minify css
-		.pipe(gulp.dest(styles.dist))			// output css and sourcemaps
-		.pipe(plugins.rename({suffix: '.min'})) // rename file to main.min.css
+	gulp.src(styles.src)
+		.pipe(plugins.sass())
+		.pipe(plugins.minifyCss())
+		.pipe(gulp.dest(styles.dist))
+		.pipe(plugins.rename({suffix: '.min'}))
 
 });
 
@@ -93,7 +91,6 @@ gulp.task('images-dist', function() {
 // move other files for build
 gulp.task('move', function() {
 
-	// move all php files and style.css
 	gulp.src(['src/**/*.php', 'src/style.css'])
 		.pipe(gulp.dest('build/'));
 
@@ -102,7 +99,6 @@ gulp.task('move', function() {
 // move other files for dist
 gulp.task('move-dist', function() {
 
-	// move all php files and style.css
 	gulp.src(['src/**/*.php', 'src/style.css'])
 		.pipe(gulp.dest('dist/'));
 
@@ -121,17 +117,9 @@ gulp.task('clean', function() {
 gulp.task('watch', function() {
 
 	// plugins.livereload.listen();
-
-	// watch for sass style changes and run styles
 	gulp.watch('src/assets/styles/**/*.scss', ['styles']);
-
-	// watch for php file changes and run move
 	gulp.watch(['src/**/*.php', 'src/style.css'], ['move']);
-
-	// watch for js file changes and run scripts
 	gulp.watch('src/assets/scripts/**/*.js', ['scripts']);
-
-	// watch for changes to the images and run images
 	gulp.watch('src/assets/images/**/*', ['images']);
 
 });
